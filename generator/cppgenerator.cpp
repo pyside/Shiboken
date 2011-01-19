cde05a92336a9f5fee1d8d1f591c89dd1ee70328
Renato Araujo Oliveira Filho <renato.filho@openbossa.org>
2011-01-19 09:38:17 -0300

    Fix windows compilation.
    Reviewer: Hugo Parente <hugo.lima@openbossa.org>
              Luciano Wolf <luciano.wolf@openbossa.org>

--- generator/cppgenerator.cpp
+++ generator/cppgenerator.cpp
@@ -2721,8 +2721,7 @@ void CppGenerator::writeGetterFunction(QTextStream& s, const AbstractMetaField* 
 
 #ifdef AVOID_PROTECTED_HACK
     if (metaField->isProtected())
-        cppField = QString("(%1(%2*)%3)->%4()")
-            .arg(useReference ? '&' : ' ')
+        cppField = QString("((%1*)%2)->%3()")
             .arg(wrapperName(metaField->enclosingClass()))
             .arg(cpythonWrapperCPtr(metaField->enclosingClass(), "self"))
             .arg(protectedFieldGetterName(metaField));

4b3a9db31192820e9d36cdd901be5c46dfa491e6
Hugo Parente Lima <hugo.pl@gmail.com>
2011-02-02 19:04:16 -0200

    Don't write return heuristic when there's a parent tag with the returned object involved.
    Reviewer: Renato Araújo <renato.filho@openbossa.org>
              Luciano Wolf <luciano.wolf@openbossa.org>

--- generator/cppgenerator.cpp
+++ generator/cppgenerator.cpp
@@ -2196,6 +2196,10 @@ void CppGenerator::writeMethodCall(QTextStream& s, const AbstractMetaFunction* f
         }
     }
 
+    // If there's already a setParent(return, me), don't use the return heuristic!
+    if (func->argumentOwner(func->ownerClass(), -1).index == 0)
+        hasReturnPolicy = true;
+
     if (!ownership_mods.isEmpty()) {
         s << endl << INDENT << "// Ownership transferences." << endl;
         foreach (ArgumentModification arg_mod, ownership_mods) {
@@ -2206,7 +2210,7 @@ void CppGenerator::writeMethodCall(QTextStream& s, const AbstractMetaFunction* f
                 break;
             }
 
-            if (arg_mod.index == 0)
+            if (arg_mod.index == 0 || arg_mod.owner.index == 0)
                 hasReturnPolicy = true;
 
             // The default ownership does nothing. This is useful to avoid automatic heuristically
@@ -3872,12 +3876,8 @@ bool CppGenerator::writeParentChildManagement(QTextStream& s, const AbstractMeta
             childVariable = usePyArgs ? "pyargs["+QString::number(childIndex-1)+"]" : "arg";
 
         s << INDENT << "Shiboken::Object::setParent(" << parentVariable << ", " << childVariable << ");\n";
-
         return true;
     }
-
-    if (argIndex == 0 && useHeuristicPolicy)
-        writeReturnValueHeuristics(s, func);
 
     return false;
 }
@@ -3890,7 +3890,10 @@ void CppGenerator::writeParentChildManagement(QTextStream& s, const AbstractMeta
     //  0    = self
     //  1..n = func. args.
     for (int i = -1; i <= numArgs; ++i)
-        writeParentChildManagement(s, func, i, i == 0 ? useHeuristicForReturn : true);
+        writeParentChildManagement(s, func, i, useHeuristicForReturn);
+
+    if (useHeuristicForReturn)
+        writeReturnValueHeuristics(s, func);
 }
 
 void CppGenerator::writeReturnValueHeuristics(QTextStream& s, const AbstractMetaFunction* func, const QString& self)
